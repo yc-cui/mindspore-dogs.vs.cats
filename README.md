d464cd24f1df495dd3b42d68362783236b27e6be
cugcuiyc <cugcuiyc@126.com>
2022-05-31 15:44:43 +0800

    README

---
+++ README.md
@@ -0,0 +1,41 @@
+# MindSpore 构建 GoogLeNet 图像分类
+
+GoogLeNet, InceptionV3, SVM
+
+## Requirements
+
+`pip install -r requirements.txt`
+
+## Dataset
+
+[https://www.kaggle.com/c/dogs-vs-cats](https://www.kaggle.com/c/dogs-vs-cats)
+
+## Training
+
+`python -m model_name -e epoch -w`
+
+## Results
+
+[https://wandb.ai/cugcuiyc/DLFIN](https://wandb.ai/cugcuiyc/DLFIN)
+
+
+![svm tune](figs/svm.png)
+
+![googlenet tune](figs/googlenet.png)
+
+![googlenet tune](figs/googlenetacc.png)
+
+![wrong](figs/wrong_imp.png)
+
+## Pretrained models
+
+链接：https://pan.baidu.com/s/1IZiPRT21dyOgOJBFQPwzXg 
+提取码：0cd0
+
+| 模型 | 正确率 |
+|  :----:|  :----: |
+| model_GoogLeNet_best_param | 0.9486 |
+| model_GoogLeNet_imp_best_param | 0.9906 |
+
+## Contact
+[cugcuiyc@126.com](mailto:cugcuiyc@126.com)
--- main.py
+++ main.py
@@ -13,9 +13,6 @@ from utils.model_utils import setup_seed
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--model", "-m", default="GoogLeNet_imp")
-# parser.add_argument("--learning_rate", "-l", default=0.001, type=float)
-# parser.add_argument("--hidden", "-hd", default=256, type=int)
-# parser.add_argument("--batchsize", "-b", default=512, type=int)
 parser.add_argument("--epoch", "-e", default=20, type=int)
 parser.add_argument("--wandb", "-w", default=False, action="store_true")
 
@@ -24,13 +21,9 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
     setup_seed(22)
-    # wandb.init()
     cfg = get_cfg_defaults()
     cfg.MODEL.NAME = args.model
     cfg.WANDB.OPEN = args.wandb
-    # cfg.TRAIN.LEARNING_RATE = args.learning_rate
-    # cfg.TRAIN.HIDDEN_SIZE = args.hidden
-    # cfg.TRAIN.BATCH_SIZE = args.batchsize
     cfg.TRAIN.NUM_EPOCH = args.epoch
     print(cfg)
     print()
